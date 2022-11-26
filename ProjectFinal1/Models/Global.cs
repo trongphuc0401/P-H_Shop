d10a1b0adcb58b100a73e45bda9703a1ed4bbc26
NGUYỄN THANH HƯƠNG <93818052+hl2802@users.noreply.github.com>
2022-11-27 01:45:55 +0700

    add_file_Global

---
+++ ProjectFinal1/Models/Global.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ProjectFinal1.Models;
+namespace ProjectFinal1.Models
+{
+    public class Global
+    {
+        /// <summary>
+        /// function to get product 
+        /// </summary>
+        /// <returns></returns>
+        public static List<PRODUCT> GetPRODUCTs()
+        {
+            List<PRODUCT> pr = new List<PRODUCT>();
+            DbContext db = new DbContext("name=MEN_FASHIONSEntities");
+            // get data
+            pr = db.Set<PRODUCT>().ToList();
+            return pr;
+        }
+        /// <summary>
+        /// function to get product type
+        /// </summary>
+        /// <returns></returns>
+        public static List<LOAISP> GetLOAISPs()
+        {
+            return new DbContext("name=MEN_FASHIONSEntities").Set<LOAISP>().ToList();
+        }
+        
+        public static List<ARTICLE> GetARTICLEs() 
+        {
+            return new DbContext("name=MEN_FASHIONSEntities").Set<ARTICLE>().ToList();
+        }
+        public static List<BRAND> GetBRANDs()
+        {
+            return new DbContext("name=MEN_FASHIONSEntities").Set<BRAND>().ToList();
+        }
+    }
+}
