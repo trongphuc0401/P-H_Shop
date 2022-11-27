30701681ce807709340c9621ab3798eb58497056
NGUYỄN THANH HƯƠNG <93818052+hl2802@users.noreply.github.com>
2022-11-28 01:15:24 +0700

    update product

--- ProjectFinal1/Controllers/Single_productController.cs
+++ ProjectFinal1/Controllers/Single_productController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectFinal1.Controllers;
+using ProjectFinal1.Models;
 
 namespace ProjectFinal1.Controllers
 {
@@ -12,6 +14,16 @@ namespace ProjectFinal1.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Index(string ID_PR)
+        { 
+            
+            MEN_FASHIONSEntities db = new MEN_FASHIONSEntities();
+
+            PRODUCT pr = db.PRODUCTS.Where(x => x.ID_PR == ID_PR).First<PRODUCT>();
+                ViewData["Single_Product"] = pr;
+;            return View();
         }
     }
 }
